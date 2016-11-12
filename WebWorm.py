9d9c71589abab886bf85fef03538fa76f3e7a219
Weili An <China_Aisa@live.com>
2016-11-13 00:01:43 +0800

    Using Thread, finished, tesing

--- WebWorm.py
+++ WebWorm.py
@@ -10,28 +10,32 @@ reader=csv.DictReader(database)
 imgdict=dict()
 maplist=set()
 def grabber(Database,url,place):
-    if place['State'] == "PR":
+    if place['State'] == "PR" or "HI":
         screenlock.acquire()
         print("[-] "+i["Zipcode"]+" Not in list")
         screenlock.release()
         return
     else:
-        parameters={"zip":place['Zipcode']}
-        map=requests.post(mapRequestUrl,params=parameters)
-        map=bs4.BeautifulSoup(map.text,"html.parser")
-        imgPage=map.find("img",id="imgMap")["src"]
-        imguri="https://www.ups.com"+imgPage
-        if imguri in maplist:
-            return
-        else:
+        try:
+            parameters={"zip":place['Zipcode']}
+            map=requests.post(mapRequestUrl,params=parameters)
+            map=bs4.BeautifulSoup(map.text,"html.parser")
+            imgPage=map.find("img",id="imgMap")["src"]
+            imguri="https://www.ups.com"+imgPage
+            if imguri in maplist:
+                return
+            else:
 
-            maplist.add(imguri)
-            imgdir="./map/"+place["State"]+place["Zipcode"]+".gif"
-            urlretrieve(imguri,imgdir)
+                maplist.add(imguri)
+                imgdir="./map/"+place["State"]+place["Zipcode"]+".gif"
+                urlretrieve(imguri,imgdir)
+                screenlock.acquire()
+                print("[+] Download successfully on Map "+place["Zipcode"])
+                screenlock.release()
+        except TimeoutError or TypeError as err:
             screenlock.acquire()
-            print("[+] Download successfully on Map "+place["Zipcode"])
+            print("[-] Zipcode "+place["Zipcoe"]+" area cannot use as warehouse. Err code"+str(err))
             screenlock.release()
-
 for i in reader:
     #print(i['Zipcode'])
     grab=Thread(target=grabber,args=(database,mapRequestUrl,i))
