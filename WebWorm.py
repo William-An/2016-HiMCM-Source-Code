3bd316f204af488a57d09ac7ff38bb6ff68aa72a
Weili An <China_Aisa@live.com>
2016-11-12 21:49:58 +0800

    Finish grabbing page

--- WebWorm.py
+++ WebWorm.py
@@ -6,9 +6,14 @@ mapRequestPage="https://www.ups.com/maps/"
 reader=csv.DictReader(database)
 for i in reader:
     #print(i['Zipcode'])
-    parameters={zip:"i"}
+    parameters={"zip":'03101'}
     map=requests.post(mapRequestPage,params=parameters)
     print(map.text)
+    map=bs4.BeautifulSoup(map.text,"html.parser")
+    print(map)
+    input()
+    img=map.find_all("img",id="imgMap")
+    print(img)
 
 
 
