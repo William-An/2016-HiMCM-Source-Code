f5e0fdd833bd93045b79e5d857af69809f4cd121
Weili An <China_Aisa@live.com>
2016-11-12 23:41:18 +0800

    Prototype Webworm, improved speed

--- WebWorm.py
+++ WebWorm.py
@@ -1,22 +1,37 @@
+from urllib.request import urlretrieve
+from threading import *
 import bs4
 import requests
 import csv
-from urllib.request import urlretrieve
+
 database=open("free-zipcode-database-Primary.csv")
-mapRequestPage="https://www.ups.com/maps/"
+mapRequestUrl="https://www.ups.com/maps/"
 reader=csv.DictReader(database)
 imgdict=dict()
+maplist=set()
+def grabber(Database,url,place):
+    if place['State'] == "PR":
+        print("[-] Location Not in list")
+        return
+    else:
+        parameters={"zip":place['Zipcode']}
+        map=requests.post(mapRequestUrl,params=parameters)
+        map=bs4.BeautifulSoup(map.text,"html.parser")
+        imgPage=map.find("img",id="imgMap")["src"]
+        imguri="https://www.ups.com"+imgPage
+        if imguri in maplist:
+            return
+        else:
+            maplist.add(imguri)
+        imgdir="./map/"+place["State"]+place["Zipcode"]+".gif"
+        urlretrieve(imguri,imgdir)
+        print("[+] Download successfully on Map "+place["Zipcode"])
+
 for i in reader:
     #print(i['Zipcode'])
-    parameters={"zip":i['Zipcode']}
-    map=requests.post(mapRequestPage,params=parameters)
-    map=bs4.BeautifulSoup(map.text,"html.parser")
-    imgPage=map.find("img",id="imgMap")["src"]
-    imguri="https://www.ups.com"+imgPage
-    imgdir="./map/"+i["State"]+i["Zipcode"]+".gif"
-    urlretrieve(imguri,imgdir)
-    print("[+] Download successfully on Map "+i["Zipcode"])
-
+    #grab=Thread(target=grabber,args=(database,mapRequestUrl,i))
+    #grab.start()
+    grabber(database,mapRequestUrl,i)
 
 
 
