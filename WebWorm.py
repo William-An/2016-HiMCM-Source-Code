58afa909f47120d3efb6b057707127da56dd11d3
Weili An <China_Aisa@live.com>
2016-11-12 23:06:24 +0800

    Prototype Webworm finished, seeking for improving speed

--- WebWorm.py
+++ WebWorm.py
@@ -1,19 +1,22 @@
 import bs4
 import requests
 import csv
+from urllib.request import urlretrieve
 database=open("free-zipcode-database-Primary.csv")
 mapRequestPage="https://www.ups.com/maps/"
 reader=csv.DictReader(database)
+imgdict=dict()
 for i in reader:
     #print(i['Zipcode'])
-    parameters={"zip":'03101'}
+    parameters={"zip":i['Zipcode']}
     map=requests.post(mapRequestPage,params=parameters)
-    print(map.text)
     map=bs4.BeautifulSoup(map.text,"html.parser")
-    print(map)
-    input()
-    img=map.find_all("img",id="imgMap")
-    print(img)
+    imgPage=map.find("img",id="imgMap")["src"]
+    imguri="https://www.ups.com"+imgPage
+    imgdir="./map/"+i["State"]+i["Zipcode"]+".gif"
+    urlretrieve(imguri,imgdir)
+    print("[+] Download successfully on Map "+i["Zipcode"])
+
 
 
 
